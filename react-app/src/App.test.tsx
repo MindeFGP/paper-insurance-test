0b7e77ea1a88ccd554ae468e9bb35f98a34534a8
Minde Luk <mindefgp@gmail.com>
2020-02-16 19:18:27 +0000

    Use app data custom hook in app component and added a test

--- react-app/src/App.test.tsx
+++ react-app/src/App.test.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 import App from './App';
 import { shallow } from 'enzyme';
 
+jest.mock("./utils/useAppData", () => ({
+  useAppData: jest.fn().mockImplementation(() => {
+    return {
+      areUsersLoaded: false,
+      arePostsLoaded: false,
+      areCommentsLoaded: false,
+      users: [],
+      posts: [],
+      comments: []
+    }
+  })
+}))
+
 test("renders without crashing", () => {
   const app = shallow(<App />)
   expect(app.exists()).toBe(true)
+});
+
+test("renders failure message when data not loaded", () => {
+  const app = shallow(<App />)
+  expect(app.find("div").first().text()).toBe("Failed to load data")
 });
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './App.css';
+import { useAppData } from './utils/useAppData';
 
 const App = () => {
+  const appData = useAppData()
+
+  if (!appData.areUsersLoaded || !appData.arePostsLoaded || !appData.areCommentsLoaded) {
+    return (
+      <div>Failed to load data</div>
+    )
+  }
+
   return (
     <div></div>
   )

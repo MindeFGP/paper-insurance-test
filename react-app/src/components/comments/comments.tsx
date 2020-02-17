553450e8ea55f76c83272dc968ffd0a069a43478
MindeFGP <mindefgp@gmail.com>
2020-02-17 13:24:52 +0000

    Added a add comment section and styled it

--- react-app/src/components/comments/comments.tsx
+++ react-app/src/components/comments/comments.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { Comment as CommentModel } from "../../models/comment/comment"
 import { Comment } from "../comment/comment"
+import { Form, FormGroup, Input, Button, Label } from "reactstrap"
+import "./comments.css"
 
 export interface CommentsProps {
     heading: string
@@ -9,9 +11,34 @@ export interface CommentsProps {
 }
 
 export const Comments = React.memo((props: CommentsProps) => {
+    const initialState = { commentInputValue: "" }
+    const [ state, setState ] = useState(initialState)
+
+    const handleCommentInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({ commentInputValue: event.target.value })
+    }
+
+    const handleCommentSubmit = () => {
+        if (state.commentInputValue !== "") {
+            props.onSubmit(state.commentInputValue)
+            setState({ commentInputValue: "" })
+        }
+    }
+
     return (
         <div>
             <h2>{props.heading}</h2>
+
+            <div className="add-comment-section">
+                <Form>
+                    <Label>Add comment:</Label>
+                    <FormGroup>
+                        <Input type="text" value={state.commentInputValue} onChange={handleCommentInputChange} />
+                    </FormGroup>
+                    <Button className="btn btn-primary" onClick={handleCommentSubmit}>Add comment</Button>
+                </Form>
+            </div>
+
             {props.listOfCommentModels.map(commentModel => {
                 return (
                     <Comment key={commentModel.id} commentModel={commentModel} />
